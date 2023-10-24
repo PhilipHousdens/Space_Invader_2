0f4c6d582776d8a84bb8e78a16679f9c177674ba
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-10-24 14:44:58 +0700

    Setting up for enemy ship movement

--- src/main/java/com/opcode/spaceinvader2/entity/EnemyShip.java
+++ src/main/java/com/opcode/spaceinvader2/entity/EnemyShip.java
@@ -1,14 +1,18 @@
 package com.opcode.spaceinvader2.entity;
 
 import com.opcode.spaceinvader2.Launcher;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 import java.util.Objects;
 
 public class EnemyShip extends Pane {
     private ImageView shipImageView;
+    private SequentialTransition transition;
 
     public EnemyShip() {
         // Load the image for the player's ship
@@ -20,5 +24,24 @@ public class EnemyShip extends Pane {
         // Set initial position
         setTranslateX(100);
         setTranslateY(100);
+
+        // Set up movement animations
+        setupMovementAnimations();
+    }
+    private void setupMovementAnimations() {
+        // Movement to the right
+        TranslateTransition moveRight = new TranslateTransition(Duration.seconds(2), this);
+        moveRight.setFromX(0);
+        moveRight.setToX(450);
+
+        // Movement to the left (return journey)
+        TranslateTransition moveLeft = new TranslateTransition(Duration.seconds(2), this);
+        moveLeft.setFromX(450);
+        moveLeft.setToX(0);
+
+        // Sequentially play the animations
+        transition = new SequentialTransition(moveRight, moveLeft);
+        transition.setCycleCount(TranslateTransition.INDEFINITE);
+        transition.play();
     }
 }
